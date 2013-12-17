be89b34951f87219778205ccf00af728dbc6a3df
misteriepie@gmail.com <misteriepie@gmail.com@04390409-adc5-fbf6-38a9-9ff6f64e9bbc>
2013-12-17 18:26:56 +0000

    verplaatst naar trunk

---
+++ BeestTest/src/beesttest/Beest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor. 
+ */
+package beesttest;
+
+/**
+ *
+ * @author Bart Janisse
+ */
+public class Beest {
+
+    IGedrag gedrag;
+    
+    public Beest(IGedrag g) {
+        this.gedrag = g;
+    }
+
+}
