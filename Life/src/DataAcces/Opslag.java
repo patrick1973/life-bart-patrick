df926d1568596e410fb03a081b6e911f15332375
Famjanisse@gmail.com <Famjanisse@gmail.com@04390409-adc5-fbf6-38a9-9ff6f64e9bbc>
2013-12-23 20:05:38 +0000

    Versie met eerste MVC

---
+++ Life/src/DataAcces/Opslag.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataAcces;
+
+/**
+ *
+ * @author Bart Janisse
+ */
+public class Opslag {
+    
+}
