c47f3cf436e8fecd63536b91a396de52544287ba
Famjanisse@gmail.com <Famjanisse@gmail.com@04390409-adc5-fbf6-38a9-9ff6f64e9bbc>
2013-12-29 19:02:53 +0000

    Commit na samenkomen en build fouten 2

--- trunk/Life/src/Views/SimulatieControllerView.java
+++ trunk/Life/src/Views/SimulatieControllerView.java
@@ -116,14 +116,15 @@ public class SimulatieControllerView extends javax.swing.JPanel {
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btnSaveSimultation)
-                            .addComponent(btnExitSimulation)
-                            .addComponent(btnPauzeSimulation)))
+                            .addComponent(btnExitSimulation)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(20, 20, 20)
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jSliderSimulationSpeed, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(40, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnPauzeSimulation)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
